71d1a00cd53b5299b1744c636fbaee4699f8640e
LemoZun <marrzun@gmail.com>
2024-10-08 14:44:07 +0900

    feat : raycast 로 터치 위치 prefab 생성

---
+++ Assets/Scripts/Painter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Painter : MonoBehaviour
+{
+    [SerializeField] public GameObject paintPrefab;
+    private ARRaycastManager raycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+        if (raycastManager == null)
+        {
+            Debug.LogError("ARRaycastManager 찾지 못함");
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Debug.Log($"Touch phase : {touch.phase}");
+            if(touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+            {
+                Vector2 screenPos = touch.position;
+                Ray ray = Camera.main.ScreenPointToRay(screenPos);
+
+                if(raycastManager.Raycast(ray,hits))
+                {
+                    Vector3 pos = hits[0].pose.position;
+                    Quaternion rot = hits[0].pose.rotation;
+                    Instantiate(paintPrefab, pos, rot);
+                }
+            }
+        }
+    }
+}
